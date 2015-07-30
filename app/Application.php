89710c76334fdf36323d4f8a1680df0d06c35f4b
John P Bloch <johnpbloch@gmail.com>
2015-07-30 09:37:36 -0500

    Add a name and version for our command

--- app/Application.php
+++ app/Application.php
@@ -6,4 +6,10 @@ use Symfony\Component\Console\Application as BaseApplication;
 
 class Application extends BaseApplication {
 
+	const VERSION = '0.1.0';
+
+	public function __construct( $name = null, $version = null ) {
+		parent::__construct( $name ?: 'WordPress Updater', $version ?: self::VERSION );
+	}
+
 }
