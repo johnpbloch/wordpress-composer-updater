48724024e363a0e96cd342c5dac00015f87bb2e6
John P Bloch <johnpbloch@gmail.com>
2015-07-30 10:15:43 -0500

    Inject the factory into the check command

--- bootstrap.php
+++ bootstrap.php
@@ -2,12 +2,13 @@
 
 use JPB\WordPress\Updater\Application;
 use JPB\WordPress\Updater\Commands\CheckUpstreamRevisions;
+use JPB\WordPress\Updater\Repository\Factory as RepositoryFactory;
 
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
 
 $app = new Application();
-$app->add( new CheckUpstreamRevisions() );
+$app->add( new CheckUpstreamRevisions( new RepositoryFactory() ) );
 
 return $app;
