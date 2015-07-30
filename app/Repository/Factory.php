41cd3dce29cc36638c8b876adcac117884138b58
John P Bloch <johnpbloch@gmail.com>
2015-07-30 10:12:28 -0500

    Add a repo interface and a factory

---
+++ app/Repository/Factory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace JPB\WordPress\Updater\Repository;
+
+class Factory {
+
+	/**
+	 * @param string $remote
+	 *
+	 * @return RepositoryInterface
+	 */
+	function create( $remote ) {
+	}
+
+}
---
+++ app/Repository/RepositoryInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace JPB\WordPress\Updater\Repository;
+
+interface RepositoryInterface {
+
+}
